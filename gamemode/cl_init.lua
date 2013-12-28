793b6a2d04d367368e66996d8bb610afa1a167b0
LennyPenny <l.bernhardt@live.de>
2013-12-29 00:22:09 +0100

    added the view model hands to the gm

--- gamemode/cl_init.lua
+++ gamemode/cl_init.lua
@@ -1,3 +1,15 @@
 include('shared.lua')
 include('core/core.lua')
 include('modules/modules.lua')
+
+
+function GM:PostDrawViewModel( vm, ply, weapon )
+
+	if ( weapon.UseHands || !weapon:IsScripted() ) then
+
+		local hands = LocalPlayer():GetHands()
+		if ( IsValid( hands ) ) then hands:DrawModel() end
+
+	end
+
+end
