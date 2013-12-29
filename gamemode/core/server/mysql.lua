a7831d8f994afd1a62974ec036457448178449cc
AlexSwift <alex@carlingfordgroup.co.uk>
2013-12-29 13:04:14 +0000

    MySQL core module
    Quick untested module. In theory should work as it is only a wrapper
    with a queue solution.

---
+++ gamemode/core/server/mysql.lua
@@ -0,0 +1,50 @@
+require('tmysql4'
+
+core.MySQL = {}
+core.MySQL.Authentication = {}
+core.MySQL.Queries = {}
+core.MySQL.Loaded = false
+core.MySQL.InProg = false
+
+function core.MySQL.Initialize()
+	local host = core.MySQL.Authentication['host'] or '127.0.0.1'
+	local user = core.MySQL.Authentication['user'] or 'root'
+	local pass = core.MySQL.Authentication['pass'] or ''
+	local daba = core.MySQL.Authentication['daba'] or 'main'
+	local port = core.MySQL.Authentication['port'] or 3306
+
+	tmysql.initialize(host, user, pass, daba, port)
+
+	--Test query to check if connected?
+
+	core.MySQL.Loaded = true
+end
+
+function core.MySQL.Query( q, callback, flags, vargs)
+	table.insert(core.MySQL.Queries, {q, callback, flags, vargs})
+	if not core.MySQL.InProg then
+		core.MySQL.Process()
+	end
+end
+
+local function table.shift( tabl , n)
+	for k,v in ipairs(tabl) do
+		tabl[k-n] = v
+	end
+	return tabl
+end
+
+function core.MySQL.Process()
+	table.shift( core.MySQL.Queries , 1)
+	if core.MySQL.Queries[1] == nil then
+		core.MySQL.InProg = false
+		return
+	end
+	local function callback(...)
+		local tabl = {...}
+		core.MySQL.Process()
+		core.MySQL.Queries[1][2](unpack(tabl))
+	end
+	tmysql.query(core.MySQL.Queries[1][1], callback, core.MySQL.Queries[1][3], core.MySQL.Queries[1][4])
+end
+
