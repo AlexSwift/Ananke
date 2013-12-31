1d288085453ea671e605e083f09e9ad154fc9805
AlexSwift <alex@carlingfordgroup.co.uk>
2013-12-31 00:03:16 +0000

    Temporary fix for stack traces

--- gamemode/core/shared/debug.lua
+++ gamemode/core/shared/debug.lua
@@ -5,7 +5,7 @@ core.debug.timestamp = core.debug.enabled and os.time() or 0
 
 function core.debug.Add()
 	if !core.debug.enabled then return end
-	local info = debug.getinfo(level, "nSl")
+	local info = debug.getinfo(1, "nSl")
 	core.debug.data[info.short_src] = core.debug.data[info.short_src] or {}
 	core.debug.data[info.short_src][info.linedefined] = core.debug.data[info.short_src][info.linedefined] or {}
 	local i = core.debug.data[info.short_src][info.linedefined][1] + 1 or 1
