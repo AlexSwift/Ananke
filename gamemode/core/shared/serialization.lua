01b9708c4507a3d0cecd6b5331c2bbedb7fa1666
AlexSwift <alex@carlingfordgroup.co.uk>
2014-01-01 15:58:51 +0000

    Small update

--- gamemode/core/shared/serialization.lua
+++ gamemode/core/shared/serialization.lua
@@ -31,21 +31,21 @@ function core.serialization.initialize()
 						end
 						,function( s_data )
 							local t = string.Explode( '' , s_data )
-							print(string.byte(t[#t]))
-							return ( (#t - 2)*(255-30) + string.byte(t[#t]) - 31)
+							return ( (#t - 2)*255 + string.byte(t[#t]) - 31)
 						end},
 		[4] = {'Angle'	,function( data )
 							local r = ''
-							r = r .. core.serialization.translations['start_number'][2](data.p) .. string.char(2 * #core.serialization.translations + 1)
-							r = r .. core.serialization.translations['start_number'][2](data.y) .. string.char(2 * #core.serialization.translations + 1)
+							r = r .. core.serialization.translations['start_number'][2](data.p) .. string.char(12)
+							r = r .. core.serialization.translations['start_number'][2](data.y) .. string.char(12)
 							r = r .. core.serialization.translations['start_number'][2](data.r)
 							return r
 						end
 						,function( s_data )
-							local t = string.explode( string.char( 2 * #core.serialization.translations) , s_data )
+							print(s_data,string.char( 12 ))
+							local t = string.Explode( string.char( 12 ) , s_data )
 							local tabl = {}
 							for i = 1,3 do
-								tabl[i] = core.serialization.translations['number'][2](t[i])
+								tabl[i] = (core.serialization.translations['start_number'][3](t[i]) <= 0) and (core.serialization.translations['start_number'][3](t[i]) + 255) or (core.serialization.translations['start_number'][3](t[i]))
 							end
 							local ang = Angle( unpack(tabl) )
 							return ang
@@ -83,6 +83,37 @@ function core.serialization.encode(data)
 end
 
 function core.serialization.decode(s_data)
+	/*
+	local typ = string.GetChar(s_data,1) --start_type data
+	local data = {}
+	for k,v in pairs(core.serialization.translations) do
+		if v[1] != typ then continue end
+		data = v[3](s_data)
+	end
+	*/
+
+	/*
+	local tabl = string.Explode( '' , s_data )
+	local buffer1 = {} -- [depth] = {startpos , endpos}
+	local buffer2 = {}
+	local j = 1 --depth
+	for i = 1,#tabl do
+		if tabl[i] == string.char(3) then		--New table
+			j = j + 1
+			buffer2[j] = i
+
+		elseif tabl[i] == string.char(4) then	--End of table
+			j = j - 1
+			table.insert(buffer1,{buffer2[j],i})
+			buffer2[j] = nil
+		end
+	end
+	tabl = nil
+	buffer2[j] = nil
+	for k,v in pairs(s_data) do
+		local tabl = string.Explode( string.char( 2 * #core.serialization.translations) , string.trim( s_data , v[1] , v[2] ))
+	end
+	*/
 
 	local typ = string.byte(string.GetChar(s_data,1)) --start_type data
 	s_data = string.TrimLeft(s_data, string.GetChar(s_data,1) )
