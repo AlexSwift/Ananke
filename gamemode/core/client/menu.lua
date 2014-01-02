ad176da7594b8b8b5085c801d4714e52ba830ba3
AlexSwift <alex@carlingfordgroup.co.uk>
2014-01-02 21:32:49 +0000

    Quick update

---
+++ gamemode/core/client/menu.lua
@@ -0,0 +1,83 @@
+core.menu = {}
+core.menu.Enabled = false
+
+core.menu.gui = {}
+core.menu.gui.__index = {}
+
+function core.menu.gui.New()
+	local tabl = {}
+	return setmetatable(tabl,table.Copy(core.menu.gui))
+end
+
+function core.menu.gui.Get( name )
+	return setmetatable( { } , _UI[name] )
+end
+
+
+function core.menu.gui:Register()
+	_UI[self.name] = table.Copy(self)
+end
+
+function core.menu.gui:AddParam( ... )
+	local args = {...}
+	if type(args[1]) == 'table' then
+		for k,v in pairs(args[1]) do
+			self[key] = value
+		end
+	else
+		self[args[1]] = args[2]
+	end
+end
+
+function core.menu.gui:GetParam( key , ... ) --Key, Default
+	local args = {...}
+	local ret = self[key] and self[key] or args[1]
+	return ret
+end
+
+function core.menu.gui:DraW()
+	--Override this function
+end
+
+function core.menu.Initialise()
+
+	local prefix = CLIENT and "lua_temp" or "gamemodes/wp_base/gamemode"
+
+	local f,d = file.Find( prefix .. "/core/client/gui/*.lua", "GAME" )
+
+	print('\tLoading Gui:')
+
+	for k,v in pairs(f) do
+		print('\t\tLoading ' .. v)
+		include('gui/'..v)
+
+	end
+
+	hook.Add( 'Draw' , 'core.menu.draw' , function( )
+		if !core.menu.Enabled then return end
+		GAMEMODE:DrawMenu( )
+		return
+	end
+
+end
+
+function core.menu.Enable( )
+	core.menu.Enabled = true
+end
+
+function core.menu.Disable( )
+	core.menu.Enabled = false
+end
+
+
+core.menu.Initialise()
+
+--				Usage:
+-- local gui = core.menu.gui.Get( 'text' )
+-- gui:AddParam( 'x' , 50 )
+-- gui:AddParam( 'y' , 50 )
+-- gui:AddParam( 'Text' , 'Main Menu' )
+-- <Other params >
+-- gui:Draw()
+-- gui = nil
+
---
+++ gamemode/modules/admin/init.lua
@@ -0,0 +1,49 @@
+Admin = {}
+Admin.plugins = {}
+Admin.plugins.__index = Admin.plugins
+
+
+local _PLUGINS = {}
+
+function Admin.plugins.New()
+	local tabl = {}
+	return setmetatable(tabl,table.Copy(Admin.plugins))
+end
+
+function Admin.plugins:Register()
+	_PLUGINS[self.Name] = table.Copy(self)
+	local cm = chatcommands.New(string.TrimRight(v,'.lua')
+	--Register args here
+	cm:Register()
+
+end
+
+function Admin.plugins.GetByName(name)
+	return _PLUGINS[name]
+end
+
+function Admin.plugins.Initialise()
+
+	local prefix = CLIENT and "lua_temp" or "gamemodes/wp_base/gamemode"
+
+	local f,d = file.Find( prefix .. "/modules/admin/plugins/*.lua", "GAME" )
+
+	print('\tLoading plugins:')
+
+	for k,v in pairs(f) do
+		if SERVER then
+
+			print('\t\tLoading ' .. v)
+			AddCSLuaFile('plugins/'..v)
+			include('plugins/'..v)
+
+		else
+			print('\t\tLoading ' .. v)
+			include('plugins/'..v)
+		end
+	end
+
+end
+
+
+Admin.plugins.Initialise() --Load all modules.
