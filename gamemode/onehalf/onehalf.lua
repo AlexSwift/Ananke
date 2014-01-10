332af7d696dbe4d704455f7b95186ff5560b2d4b
AlexSwift <alex@carlingfordgroup.co.uk>
2014-01-10 12:19:19 +0000

    Update onehalf.lua

--- gamemode/onehalf/onehalf.lua
+++ gamemode/onehalf/onehalf.lua
@@ -1,1 +1,22 @@
 -- We trust the server, no need for thread checking.
+
+OneHalf = {}
+
+function OneHalf.Initialize()
+  
+end
+
+function OneHalf.ScreenGrab ( ply )
+  
+end
+
+function OneHalf.BanPlayer ( ply )
+  --Send cookie here
+  --Add to mysql
+  --Kick player
+end
+
+function OneHalfCheckActive ( ply )
+    --Send net here
+    --I will add a callback here too.
+end
