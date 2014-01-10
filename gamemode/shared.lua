b7ddd62b79ad4f80cb2d7cb02a8bbe85b534e10c
AlexSwift <alex@carlingfordgroup.co.uk>
2014-01-10 12:15:10 +0000

    Update shared.lua

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -1,6 +1,6 @@
+include( 'onehalf/onehalf.lua' ) --What part of 'first line' didn't you get? Locals wouldn't have worked.
+
 GM.Name 	= "wp_base"
 GM.Author 	= "TEAM WARPAC"
 GM.Email 	= ""
 GM.Website 	= ""
-
-include( 'onehalf/onehalf.lua' )
