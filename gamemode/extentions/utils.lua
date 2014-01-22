5426dcd50dc29d2c3480e5c0266b2c84359d2fe7
AlexSwift <alex@carlingfordgroup.co.uk>
2014-01-22 21:09:21 +0000

    Update utils.lua

--- gamemode/extentions/utils.lua
+++ gamemode/extentions/utils.lua
@@ -21,27 +21,21 @@ function utils.FindPlayersByName(name)
 end
 
 function utils.TimeFromString(str)
+	local coefficients = {
+		['y'] = 31556909,
+		['mo'] = 2629740,
+		['w'] = 604800,
+		['d'] = 86400,
+		['h'] = 3600,
+		['m'] = 60,
+		['s'] = 1
+	}
 	local tabl = {}
 	local tabl.total = 0
 	for v, k in string.gmatch(str, "(%d+)(%a+)") do
 		v = tonumber(v) or 0
-		if k == "y" then
-			tabl.total = tabl.total + v * 31556909 --ceiled from 31556908.8 when 1 year has 365.242 days
-		elseif k == "mo" then
-			tabl.total = tabl.total + v * 2629740 --ceiled from 2629739.52 when 1 month has 30.4368 days
-		elseif k == "w" then
-			tabl.total = tabl.total + v * 604800 --TODO Make a metric-like system for time, it makes no goddamn sense this way
-		elseif k == "d" then
-			tabl.total = tabl.total + v * 86400
-		elseif k == "h" then
-			tabl.total = tabl.total + v * 3600
-		elseif k == "m" then
-			tabl.total = tabl.total + v * 60
-		elseif k == "s" then
-			tabl.total = tabl.total + v
-		else
-			continue
-		end
+		tabl.total = tabl.total + v * coefficients[k]
+		
 		tabl[k] = v
 	end
 	return tabl
