98b5f76e7316e492ac37ef00154460cf36797dd3
LennyPenny <l.bernhardt@live.de>
2014-01-24 23:51:22 +0100

    started anankeguard

--- gamemode/modules/admin/plugins/ban.lua
+++ gamemode/modules/admin/plugins/ban.lua
@@ -2,6 +2,8 @@ local plugin = Admin.plugins.New()
 
 plugin.Name = "ban"
 
+--active stuff
+
 local function ban(ply, length, reason)
 	
 end
@@ -17,6 +19,44 @@ local function callback(ply, data)
 		ban(ply, data[2], data[3])
 	end
 end
+
+
+--passive stuff
+
+
+local function CheckBan(id)
+	local mysqlresult --query for steamid in ban list
+	if mysqlresult then
+		local tabl = {}
+		tabl.reason = mysqlresult.reason
+		tabl.data = mysqlresult.unbandate
+		tabl.timeleft = 0 --TODO DO THIS!
+		return table
+	end
+
+	mysqlresult --query for IP
+	if mysqlresult then
+		local tabl
+		tabl.suspecalt = 1
+		return tabl
+	end
+end
+
+local function AuthBan(data)
+	if data.bot == 1 then return end
+
+	local bandata = CheckBan(data.networkid)
+	if bandata.banned then
+		RunConsoleCommand("kickid", data.userid, "You are banned: "..bandata.reason.."\n".."Ban lift date: "..bandata.date.." Time remaining: "..bandata.timeleft)
+	elseif bandata.suspecalt then
+		--raise alt suspection
+	end
+end
+
+gameevent.Listen("player_connect")
+hook.Add("player_connect", "BanCheck", AuthBan)
+
+
 
 plugin['args'] = {['p'] = {'string','Default'}}
 plugin['callback'] = function(data) print(data['p']) end
