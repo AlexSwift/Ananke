56b136f4dfa1f148ee12f94d229dbf3f3141c3bd
LennyPenny <l.bernhardt@live.de>
2014-01-28 19:41:42 +0100

    removing local stuff

--- gamemode/modules/talos/init.lua
+++ gamemode/modules/talos/init.lua
@@ -1,13 +1,14 @@
 --In Greek mythology, Talos (/ˈtɑːlɵs/; Greek: Τάλως, Talōs) or Talon (/ˈtɑːlɵn/; Greek: Τάλων, Talōn) was a giant man of bronze who protected Europa in Crete from pirates and invaders.
+ananke.talos = {}
 
-local bans = {}
+ananke.talos.bans = {}
 
-local function UpdateBanTable(data)
-	bans = data
-	bans.age = CurTime()
+function ananke.talos.UpdateBanTable(data)
+	ananke.talos.bans = data
+	ananke.talos.bans.age = CurTime()
 end
 
-local function QueryBanList()
+function ananke.talos.QueryBanList()
 	core.MySQL.Query( "SELECT * FROM bans;", UpdateBanTable)
 end
 hook.Add("UpdateBanList", "MainFunc", QueryBanList )
@@ -18,23 +19,23 @@ timer.Create("BanRefresh", 10, 0, function()
 	end
 end)
 
-local function IsBanned(id)
-	if bans.id != nil then
+function ananke.talos.IsBanned(id)
+	if ananke.talos.bans.id != nil then
 		return true
 	else 
 		return false
 	end
 end
 
-local function IsBanned64(id)
-	if bans.steamid64 != nil then
+function ananke.talos.IsBanned64(id)
+	if ananke.talos.bans.steamid64 != nil then
 		return true
 	else 
 		return false
 	end
 end
 
-local function CheckFamilySharing(body)
+function ananke.talos.CheckFamilySharing(body)
 	local json = util.JSONToTable(body)
 	if json.lender_steamid != 0 then
 		if IsBanned64(json.lender_steamid) then
@@ -44,15 +45,15 @@ local function CheckFamilySharing(body)
 end
 
 local currentlyauthing = ""
-local function Auth(data)
-	if data.bot == 1 then return end
-	if IsBanned(data.networkid) then
-		RunConsoleCommand("kickid", data.userid, "You are banned: "..bans.id.reason.."\n".."Ban lift date: "..os.date("%A, %D - %T", bans.id.unban).." Time remaining: "..os.difftime(os.time(), bans.id.unban)/86400.." days")
+local function ananke.talos.Auth(data)
+	if ananke.talos.data.bot == 1 then return end
+	if ananke.talos.IsBanned(data.networkid) then
+		RunConsoleCommand("kickid", data.userid, "You are banned: "..ananke.talos.bans.id.reason.."\n".."Ban lift date: "..os.date("%A, %D - %T", ananke.talos.bans.id.unban).." Time remaining: "..os.difftime(os.time(), bans.id.unban)/86400.." days")
 		return
 	end
 	currentlyauthing = data.userid
-	http.Fetch("http://api.steampowered.com/IPlayerService/IsPlayingSharedGame/v0001/?key="..ananke.config.apikey.."&steamid="..util.SteamIDTo64(id).."&appid_playing=4000&format=json", CheckFamilySharing)
+	http.Fetch("http://api.steampowered.com/IPlayerService/IsPlayingSharedGame/v0001/?key="..ananke.config.apikey.."&steamid="..util.SteamIDTo64(id).."&appid_playing=4000&format=json", ananke.talos.CheckFamilySharing)
 end
 
 gameevent.Listen("player_connect")
-hook.Add("player_connect", "BanCheck", Auth)
+hook.Add("player_connect", "BanCheck", ananke.talos.Auth)
