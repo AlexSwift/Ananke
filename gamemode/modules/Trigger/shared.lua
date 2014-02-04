1bfb1644aebbcbfc05e9ed43f9b94de339556f55
AlexSwift <alex@carlingfordgroup.co.uk>
2014-02-04 19:53:29 +0000

    Triggers

---
+++ gamemode/modules/Trigger/shared.lua
@@ -0,0 +1,40 @@
+local modul = modules.new()
+modul.name = 'Trigger'
+
+Ananke.Trigger = {}
+Ananke.Trigger.CallBacks = {} -- ID , callback 
+
+function Ananke.Trigger.AddCallback( id , StartTouch , EndTouch )
+	
+	Ananke.Trigger.CallBacks[id] = { 	['start'] 	= StartTouch,
+										['end'] 	= EndTouch }
+	
+end
+
+function Ananke.Trigger.RemoveCallback( id )
+
+	-- CAUTION: This will break functionality
+	
+	Ananke.Trigger.CallBacks[id] = nil
+	
+end
+
+
+function Ananke.Trigger.StartTouch( trigger , ent )
+
+	local m_callback = trigger:GetKeyValues().m_callback
+	if not Ananke.Trigger.CallBacks[m_callback]['start'] then return end
+	
+	Ananke.Trigger.CallBacks[m_callback]['start']( trigger , ent )
+	
+end
+
+function Ananke.Trigger.EndTouch( trigger , ent )
+
+	local m_callback = trigger:GetKeyValues().m_callback
+	if not Ananke.Trigger.CallBacks[m_callback]['end'] then return end
+	
+	Ananke.Trigger.CallBacks[m_callback]['end']( trigger , ent )
+	
+end
+	
