50b27756d255140a86282db69d50fceda3077f40
Canadialad <mrabey11@gmail.com>
2014-02-06 16:08:08 -0500

    Updated uiBase.lua
    Changed to Simploo format
    
    Used as base object to all UIObjects within the game

--- gamemode/core/client/gui/uiBase.lua
+++ gamemode/core/client/gui/uiBase.lua
@@ -4,81 +4,177 @@
 	Description:	Base for all UIObjects in-game
 ]]--
 
-PANEL = core.menu.gui.New();
-PANEL.name = 'UIBase';
+PANEL = {}
+PANEL.name = 'uiBase';
 
-function PANEL:Init()
+class "core.menu.gui.uiBase" {
 
-	self.MouseInBounds = false
-	self.IsEnabled = false
-	self.uiParent = false
-	self.uiChildren = {}
+	meta {
+		
+	};
+
+	protected {
+		
+		SetParent = function(self, parent)
+			if self.parent then
+				self.parent:RemoveChild(self)
+			end
+			
+			self.parent = parent
+			parent:AddChild(self)
+		end;
+		
+		GetParent = function(self)
+			local ret = self.parent or nil -- Is this necessary?
+			return ret
+		end;
 	
-	self.dims = Vector( 200, 100, 0 )
-
-end
-
-function PANEL:OnCursorMoved()
-
-end
-
-function PANEL:OnCursorEntered()
-
-	self.MouseInBounds = true
-
-end
-
-function PANEL:OnCursorExited()
-
-	self.MouseInBounds = false
-
-end
-
-function PANEL:IsEnabled()
-
-	return IsEnabled
-
-end
-
-function PANEL:Enable()
-
-	self.IsEnabled = true
-
-	local children = self.children
-	if children then
-		for k, v in pairs(children) do
-			children[k].Enable()
-		end
-	end
-
-end
-
-function PANEL:Disable()
-
-	self.IsEnabled = false
-
-	local children = self.children
-	if children then
-		for k, v in pairs(children) do
-			children[k].Disable()
-		end
-	end
-
-end
-
-function PANEL:Toggle()
-
-	if self.IsEnabled then
-		return self:Disable()
-	end
-
-	return self:Enable()
+		SetPosition = function(self, x, y)
+			local pX, pY = self.parent and self.parent:GetPosition() or 0.0, 0.0 -- if no parent, we default to screen coordinates
+			
+			x = math.abs(x - pX)
+			y = math.abs(y - pY)
+			
+			self:AlignLeft(x)
+			self:AlignTop(y)
+		end;
+		
+		GetPosition = function(self)
+			local pX, pY = self.parent and self.parent:GetPosition() or 0.0, 0.0
+			local pW, pH = self.parent and self.parent:GetDimensions() or surface.ScreenWidth, surface.ScreenHeight
+			
+			return (self.relX * pW) + pX, (self.relY * pH) + pY
+		end;
+		
+		AlignTop = function(self, offset)
+			local _, pY = self.parent and self.parent:GetPosition() or 0.0, 0.0
+			local _, pHeight = self.parent and self.parent:GetDimensions() or 0.0, surface.ScreenHeight
+			
+			self.relY = (pY + offset) / pHeight
+		end;
+		
+		AlignBottom = function(self, offset)
+			local _, pY = self.parent and self.parent:GetPosition() or 0.0, 0.0
+			local _, pHeight = self.parent and self.parent:GetDimensions() or 0.0, surface.ScreenHeight
+			
+			self.relY = 1.0 - ((pY + offset) / pHeight)
+		end;
+		
+		AlignLeft = function(self, offset)
+			local pX = self.parent and self.parent:GetPosition() or 0.0
+			local pWidth = self.parent and self.parent:GetDimensions() or surface.ScreenWidth
+		
+			self.relX = (pX + offset) / pWidth
+		end;
+		
+		AlignRight = function(self, offset)
+			local pX = self.parent and self.parent:GetPosition() or 0.0
+			local pWidth = self.parent and self.parent:GetDimensions() or surface.ScreenWidth
+		
+			self.relX = 1.0 - ((pX + offset) / pWidth)
+		end;
+		
+		SetWidth = function(self, w)
+			self.width = w
+		end;
+		
+		SetHeight = function(self, h)
+			self.height = h
+		end;
+		
+		GetDimensions = function(self)
+			return self.width, self.height
+		end;
+		
+		AddChild = function(self, obj)
+			if obj == nil then return end
+			
+			table.insert(self.children, obj)
+		end;
+		
+		RemoveChild = function(self, obj)
+			for k, v in pairs(self.children) do
+				if v == obj then
+					table.remove(self.children, obj)
+				end
+			end
+		end;
+		
+		GetChildren = function(self)
+			return self.children
+		end;
+		
+		IsEnabled = function(self)
+			return self.isEnabled
+		end;
+		
+		ToggleEnabled = function(self)
+			self.isEnabled = !self.isEnabled
+			
+			if #self.children > 0 then
+				for k, v in pairs(self.children) do
+					self.children[k]:ToggleEnabled()
+				end
+			end
+		end;
+		
+		OnCursorMoved = function()
+		end;
+		
+		OnCursorEntered = function()
+		end;
+		
+		OnCursorExited = function()
+		end;
+		
+		OnMousePressed = function()
+		end;
+		
+		OnMouseReleased = function()
+		end;
+		
+		OnMouseWheeled = function()
+		end;
+	};
 	
-end
+	public {
+	
+		-- Initialise
+		__construct = function(self)
+			relX = surface.ScreenWidth * 0.45
+			relY = surface.ScreenHeight * 0.45
+			
+			width = surface.ScreenWidth * 0.1
+			height = surface.ScreenWidth * 0.1
+			
+			scaleX = 1.0
+			scaleY = 1.0
+		end;
+		
+		__finalize = function(self)
+		
+		end;
+	
+		Draw = PANEL.Draw;
+	};
+	
+	private {
+		parent = nil
+		isEnabled = false
+		children = {}
+		
+		relX = 0.0;
+		relY = 0.0;
+		width = 0.0;
+		height = 0.0;
+		scaleX = 0.0;
+		scaleY = 0.0;
+	};
+}
 
-function PANEL:Draw()
+function PANEL.Draw(self)
 	--surface.DrawRect((surface.ScreenWidth * 0.5) - 50, (surface.ScreenHeight * 0.5) - 50, 100, 100)
 end
 
-PANEL:RegisterMenuKey(KEY_M)
 PANEL:Register();
+
