e901ba5389f573dcc87973d9e67dc63a4b3f98f9
LennyPenny <l.bernhardt@live.de>
2014-02-16 14:21:46 +0100

    fixed ply:ChatPrint()

--- gamemode/modules/Networked Functions/chataddtext.lua
+++ gamemode/modules/Networked Functions/chataddtext.lua
@@ -1,41 +1,13 @@
-nwfuncs.AddChatText = {}
+Ananke.nwfuncs.AddChatText = {}
 
-if SERVER then
+Ananke.nwfuncs.AddChatText.Protocol = Ananke.core.protocol.new()
 
-	local plymeta = FindMetaTable("Player")
-
-	plymeta.oldChatPrint = plymeta.ChatPrint
-
-	function plymeta:ChatPrint(data)
-		if istable(data) then
-			local nw = network.New()
-			nw:SetProtocol(0x03)
-			nw:SetDescription("Networked chat.AddText()")
-			nw:pushdata(data)
-			nw:send()
-		else
-			return self:oldChatPrint(data)
-		end
-	end
-
-end
-
-nwfuncs.AddChatText.Protocol = Ananke.core.protocol.new()
-
-nwfuncs.AddChatText.Protocol:SetName( "nw_variables" )
-nwfuncs.AddChatText.Protocol:SetPID( 0x03 )
-nwfuncs.AddChatText.Protocol:SetType( NW_STC )		--0x01 - Server to Client
-nwfuncs.AddChatText.Protocol:SetData( "table" )
+Ananke.nwfuncs.AddChatText.Protocol:SetName( "nw_variables" )
+Ananke.nwfuncs.AddChatText.Protocol:SetPID( 0x03 )
+Ananke.nwfuncs.AddChatText.Protocol:SetType( NW_STC )		--0x01 - Server to Client
+Ananke.nwfuncs.AddChatText.Protocol:SetData({ [1] = "table" })
 
 
-nwfuncs.AddChatText.Protocol:SetCallBack(function(data)
+Ananke.nwfuncs.AddChatText.Protocol:SetCallBack(function(data)
 	chat.AddText(unpack(data))
-end)
-
-nwfuncs.AddChatText.Protocol:SetSend(function(data)
-	net.WriteTable(data)
-end)
-
-nwfuncs.AddChatText.Protocol:SetReceive(function()
-	return net.ReadTable()
 end)
--- gamemode/modules/Networked Functions/init.lua
+++ gamemode/modules/Networked Functions/init.lua
@@ -3,3 +3,21 @@ AddCSLuaFile("shared.lua")
 AddCSLuaFile("chataddtext.lua")
 
 include("shared.lua")
+
+
+
+local plymeta = FindMetaTable("Player")
+
+plymeta.oldChatPrint = plymeta.ChatPrint
+
+function plymeta:ChatPrint(data)
+	if istable(data) then
+		local nw = network.New()
+		nw:SetProtocol(0x03)
+		nw:SetDescription("Networked chat.AddText()")
+		nw:pushdata(data)
+		nw:send()
+	else
+		return self:oldChatPrint(data)
+	end
+end
