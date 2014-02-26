1f225dacce97a91c0be812cf4df1549b3a8bd160
Canadialad <mrabey11@gmail.com>
2014-02-25 23:21:56 -0500

    LinkedList Works
    Apart from Clear and Find, every other function within LinkedList acts
    as intended.  First round of tests are all complete.  There might be
    hidden bugs that I haven't caught.  Will do a follow up after Clear and
    Find are complete.

--- gamemode/extentions/linkedlist.lua
+++ gamemode/extentions/linkedlist.lua
@@ -1,241 +1,337 @@
-class "LinkedList.Node" {
+local Next = function(head, curr)
+	if not curr then
+		return head
+	elseif type(curr['nextNode']) != "nil" then
+		return curr['nextNode']
+	else
+		return nil
+	end
+end;
+
+class "LLNode" {
+
 	public {
-		__construct = function(self, value, nextNode)
-			self['value'] = value
-			self['nextNode'] = nextNode
+		__construct = function(self, val, node)
+			self['value'] = val;
+			self['nextNode'] = node;
 		end;
 		
 		__finalize = function(self)
-			self['value'] = nil;
-			self['nextNode'] = nil;
+		
 		end;
 		
-		Next = function(self)
-			return self['nextNode']
-		end;
-		
-		SetNext = function(self, nextNode)
-			self['nextNode'] = nextNode
-		end;
-		
-		Value = function(self)
-			return self['value']
-		end;
-	};
-	
-	private {
-		value = nil;
-		nextNode = nil;
+		value = '';
+		nextNode = {}
 	};
 };
 
 class "LinkedList" {
+
 	public {
 		__construct = function(self)
+			self['head'] = nil;
+			self['tail'] = nil;
 		end;
 		
-		__finalize = function(self) -- Is this necessary?
-			self:Clear()
+		__finalize = function(self)
+		
 		end;
-	
-		-- Adds given 'obj' to the head of the list.
-		-- Returns the node if successful, nil otherwise.
+		
+		-- Adds given 'obj' to the Head of the list.
+		-- Returns the head of the list if succeeded, nil otherwise.
 		AddHead = function(self, obj)
-			if(obj != nil) then
-				local node = self['Node'].new(obj, self['head'])
-				
-				self['head'] = node
-				self:IncreaseCount()
-				
-				if(self['tail'] == nil) then self['tail'] = node end
-				
-				return node
+			if type(obj) == "nil" then 
+				Error("Cannot add a Nil Object\n")
+				return nil
 			end
 			
-			return nil
+			print("\nAddHead(" .. obj .. ")\n")
+			local node = LLNode.new(obj, self['head'])
+			
+			if self:IsEmpty() then self['tail'] = node end
+			self['head'] = node
+			
+			return self['head']
 		end;
 		
-		-- Adds given 'obj' to the tail of the list.
-		-- Returns the node if successful, nil otherwise.
+		-- Adds given 'obj' to the Tail of the list.
+		-- Returns the tail of the list if succeeded, nil otherwise.
 		AddTail = function(self, obj)
-			if(obj != nil) then
-				if(head == nil) then AddHead(obj); return end
-				
-				local node = self['Node'].new(obj, nil)
-				
-				self['tail']:SetNext(node)
-				self['tail'] = node
-				
-				self:IncreaseSize()
-				
+			if type(obj) == "nil" then
+				Error("Cannot add a Nil Object")
+				return nil
+			elseif self:IsEmpty() then
+				local node = self:AddHead(obj)
 				return node
 			end
 			
-			return nil
+			print("\nAddTail(" .. obj .. ")\n")
+			local node = LLNode.new(obj, nil)
+			self['tail']['nextNode'] = node
+			self['tail'] = node
+			
+			return node
 		end;
 		
-		-- Adds given 'obj' to the list before the given 'before' node.
-		-- Returns the node added if successful, nil otherwise.
+		-- Adds given 'obj' ahead of the given 'before' LLNode.
+		-- Returns the LLNode added if succeeded.
+		-- Returns nil if 'obj' is nil or 'before' isn't of type LLNode.
 		AddBefore = function(self, obj, before)
-			if(obj != nil) then
-				if(before['Next'] == nil) then return nil end
-				if(before == self['head'] or self['head'] == nil) then
-					local node = self:AddHead(obj)
-					return node
-				end
-				
-				local curr = self['head']
-				while(curr:Next() != before) do
-					curr = curr:Next()
-				end
-				
-				if(curr != nil) then
-					local node = self['Node'].new(obj, before)
-					
-					curr:SetNext(node)
-					IncreaseSize()
-					
-					return node
-				end
+			if type(obj) == "nil" then
+				Error("\nCannot add a Nil Object\n")
+				return nil
+			elseif type(before['value']) == "nil" then
+				Error("\nGiven 'before' is not of type: LLNode\n")
+				return nil
+			elseif before == self['head'] then
+				local node = self:AddHead(obj)
+				return node
 			end
 			
-			return nil
+			print("\nAddBefore(" .. obj .. ", " .. before.value .. ")\n")
+			
+			local curr = self['head']
+			while curr['nextNode'] != before do
+				curr = curr['nextNode']
+			end
+			
+			if type(curr) != "nil" then
+				local node = LLNode.new(obj, before)
+				
+				curr['nextNode'] = node
+				
+				return node
+			end
 		end;
 		
-		-- Adds given 'obj' to the list after the given 'after' node.
-		-- Returns the node added if successful, nil otherwise.
+		-- Adds given 'obj' after the given 'after' LLNode.
+		-- Returns the LLNode added if succeeded.
+		-- Returns nil if 'obj' is nil or 'after' isn't of type LLNode.
 		AddAfter = function(self, obj, after)
-			if(obj != nil) then
-				if(after['Next'] == nil) then return nil end
-				if(self['head'] == nil) then local node = self:AddHead(obj); return node end
-				if(self['tail'] == after) then local node = self:AddTail(obj); return node end
-				
-				if(after != nil) then
-					local node = self['Node'].new(obj, after:Next())
-					
-					after:SetNext(node)
-					IncreaseSize()
-					
-					return node
-				end
+			if type(obj) == "nil" then
+				Error("\nCannot add a Nil Object\n")
+				return nil
+			elseif type(after['value']) == "nil" then
+				Error("\nGiven 'after' is not of type: LLNode\n")
+				return nil
+			elseif after == self['tail'] then
+				local node = self:AddTail(obj)
+				return node
 			end
 			
-			return nil
+			print("\nAddAfter(" .. obj .. ", " .. after.value .. ")\n")
+			
+			local node = LLNode.new(obj, after['nextNode'])
+			after['nextNode'] = node
 		end;
 		
-		-- Removes given 'obj' from the list, maintaining the link between all nodes.
-		-- Returns the next node in the list if successful, nil otherwise.
-		Remove = function(self, obj)
-			if(obj != nil) then
-				if(self['head'] == nil) then return end
-				if(self['head'] == obj) then 
-					self['head'] = obj:Next();  
-					self:DecreaseCount(); 
-					return head; 
-				end
-				
-				local prev = nil
-				local curr = self['head']
-				
-				while(curr != nil) do
-					if(curr == obj) then break end
-					
-					prev = curr
-					curr = curr:Next()
-				end
-				
-				if(curr != nil) then
-					local nextNode = curr:Next()
-					prev:SetNext(nextNode)
-					
-					if(curr == self['tail']) then self['tail'] = prev end
-					
-					obj = nil
-					self:DecreaseCount()
-					
-					return nextNode
-				end
+		-- Removes the given LLNode 'node' from the LinkedList.
+		Remove = function(self, node)
+			if self:IsEmpty() then
+				return
+			elseif node['value'] == "nil" then
+				Error("\nGiven 'node' is not of type: LLNode\n")
+				return
+			elseif node == self['head'] then
+				self:RemoveHead()
+				return
+			elseif node == self['tail'] then
+				self:RemoveTail()
+				return
 			end
 			
-			return nil
-		end;
-		
-		Clear = function(self) -- Do we need to set each individual node and its parameters to null?
-			self['head'] = nil
-			self['tail'] = nil
+			print("\nRemove(" .. node.value .. ")\n")
 			
-			self['count'] = 0
+			local curr = self['head']
+			while curr['nextNode'] != node do
+				curr = curr['nextNode']
+			end
+			
+			if type(curr) != "nil" then
+				curr['nextNode'] = node['nextNode']
+				
+				node['nextNode'] = nil
+				node['value'] = nil
+			end
 		end;
 		
-		-- Searches the list looking for 'obj'.
-		-- Returns the index where 'obj' is located if successful.
-		-- If the list is empty or 'obj' is nil, returns nil.
-		-- If 'obj' is not in the list, returns -1.
+		-- Removes the first instance of a LLNode with value 'value' from the LinkedList.
+		RemoveByValue = function(self, value)
+			if self:IsEmpty() then
+				return
+			elseif type(value) == "nil" then
+				Error("\nCannot remove Nil Object.\n")
+				return
+			elseif self['head'].value == value then
+				self:RemoveHead()
+				return
+			elseif self['tail'].value == value then
+				self:RemoveTail()
+				return
+			end
+			
+			local node = self['head']
+			while node['nextNode'].value != value do
+				node = node['nextNode']
+			end
+			
+			if type(node) != "nil" then
+				local delete = node['nextNode']
+				node['nextNode'] = delete['nextNode']
+				
+				delete['nextNode'] = nil
+				delete['value'] = nil
+			end
+		end;
+		
+		-- Removes the head of the LinkedList.
+		RemoveHead = function(self)
+			if not self:IsEmpty() then
+				print("\nRemoveHead()\n")
+			
+				local node = self['head']
+				
+				if self['head'] == self['tail'] then self['head'], self['tail'] = nil, nil end
+				
+				self['head'] = node.nextNode
+				
+				node['nextNode'] = nil
+				node['value'] = nil
+			end
+		end;
+		
+		-- Removes the tail of the LinkedList.
+		RemoveTail = function(self)
+			if not self:IsEmpty() then
+				print("\nRemoveTail()\n")
+			
+				local newTail = self['head']
+				while newTail['nextNode'] != self['tail'] do
+					newTail = newTail['nextNode']
+				end
+				
+				newTail['nextNode'] = nil
+				local delete = self['tail']
+				self['tail'] = newTail
+				
+				delete['nextNode'] = nil
+				delete['value'] = nil
+			end
+		end;
+		
+		Clear = function(self)
+		
+		end;
+		
 		Find = function(self, obj)
-			if(obj != nil) then
+			local node = self['head']
+			
+			return node
+		end;
+		
+		-- Returns whether the LinkedList is empty.
+		IsEmpty = function(self)
+			return type(self['head']) == "nil"
+		end;
+		
+		-- Returns the head of the LinkedList.
+		Head = function(self)
+			return self['head']
+		end;
+		
+		-- Returns the tail of the LinkedList.
+		Tail = function(self)
+			return self['tail']
+		end;
+		
+		-- Function used in the generic for loop to iterate through
+		-- the contents of the LinkedList.
+		-- Ex: for node in lList:Iterate() do { ... } end
+		Iterate = function(self)
+			return Next, self['head'], nil
+		end;
+		
+		static {
+			Test = function(self)
+				print("********** TESTING LINKEDLIST **********")
+				
+				local lList = LinkedList.new()
+				
+				local head = lList:AddTail("first")
+				local tail = head
+				print("Head = " .. head.value .. "\nTail = " .. tail.value)
+				
+				tail = lList:AddTail("third")
+				print("Head = " .. head.value .. "\nTail = " .. tail.value)
+				
+				local node = lList:AddAfter("second", head)
+				print("Head = " .. head.value .. "\nTail = " .. tail.value)
+				
+				local invalid = {}
+				node = lList:AddAfter("invalid", invalid)
+				print("Head = " .. head.value .. "\nTail = " .. tail.value)
+				
+				tail = lList:AddAfter("fourth", tail)
+				print("Head = " .. head.value .. "\nTail = " .. tail.value)
+				
+				lList:RemoveHead()
+				head = lList:Head()
+				print("Head = " .. head.value .. "\nTail = " .. tail.value)
+				
 				local index = 1
-				
-				if(self['head'] == nil) then return nil end
-				if(self['head'] == obj) then return index end
-				
-				local curr = self['head']
-				
-				while(curr != null) do
-					if(curr == obj) then
-						break
-					end
-					
-					curr = curr:Next()
+				print("\n\n- Printing contents -\n")
+				for node in lList:Iterate() do
+					print(index .. ") " .. node.value)
 					index = index + 1
 				end
 				
-				return curr and index or -1
-			end
-			
-			return nil
-		end;
-		
-		-- First attempts to locate the optional 'obj' given.
-		-- If no 'obj' was found, defaults the start of the search to head.
-		-- Returns the object located at the given index.
-		Get = function(self, index, obj) -- Optional anchor point EX: Get(10, head) -> head + 10
-			local anchor = Find(obj)
-			
-			if(anchor == nil) then 
-				anchor = 0
-			elseif(anchor == -1) then 
-				Ananke.core.debug.Error("LinkedList does not contain specified object.", true)
-			elseif(anchor + index > count) then 
-				Ananke.core.debug.Error("Index out of range.", true) 
-			end
-			
-			local curr = obj and obj or self['head']
-			for i=anchor, anchor + index do
-				if(curr == nil) then break end
+				lList:RemoveTail()
+				tail = lList:Tail()
+				print("Head = " .. head.value .. "\nTail = " .. tail.value)
 				
-				curr = curr:Next()
-			end
-			
-			return curr
-		end;
-	};
-	
-	protected {
-		IncreaseSize = function(self, num) -- Optional number to increase count by
-			local inc = num and num or 1
-			self['count'] = self['count'] + inc
-		end;
-		
-		DecreaseSize = function(self, num) -- Optional number to decrease count by
-			local def = num and num or 1
-			self['count'] = self['count'] - def
-		end;
+				local index = 1
+				print("\n\n- Printing contents -\n")
+				for node in lList:Iterate() do
+					print(index .. ") " .. node.value)
+					index = index + 1
+				end
+				
+				lList:RemoveByValue('third')
+				head = lList:Head()
+				tail = lList:Tail()
+				print("Head = " .. head.value .. "\nTail = " .. tail.value)
+				
+				local index = 1
+				print("\n\n- Printing contents -\n")
+				for node in lList:Iterate() do
+					print(index .. ") " .. node.value)
+					index = index + 1
+				end
+				
+				lList:RemoveHead()
+				head = lList:Head()
+				tail = lList:Tail()
+				if head == nil and tail == nil then
+					print("Head = nil\nTail = nil")
+				else
+					print("Head = " .. head.value .. "\nTail = " .. tail.value)
+				end
+				
+				local index = 1
+				print("\n\n- Printing contents -\n")
+				for node in lList:Iterate() do
+					print(index .. ") " .. node.value)
+					index = index + 1
+				end
+				
+				print("********** FINISHED LList TEST **********")
+			end;
+		};
 	};
 	
 	private {
-		head = nil;
-		tail = nil;
-		
-		count = 0;
+		head = {};
+		tail = {};
 	};
+
 };
