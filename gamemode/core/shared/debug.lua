48e97e97b08060797acc87b52ea5ff2b72e8f075
AlexSwift <alex@carlingfordgroup.co.uk>
2014-03-09 14:55:54 +0000

    Updated Debug

--- gamemode/core/shared/debug.lua
+++ gamemode/core/shared/debug.lua
@@ -1,20 +1,26 @@
 Ananke.core.debug = {}
 Ananke.core.debug.data = {}
-Ananke.core.debug.Errors = {}
+Ananke.core.debug.data.Errors = {}
+Ananke.core.debug.data.Logs = {}
+Ananke.core.debug.data.Stack = {}
 
 Ananke.core.debug.enabled = false
 Ananke.core.debug.timestamp = Ananke.core.debug.enabled and os.time() or 0
+
+DEBUG_PRINT = 1
+DEBUG_ERROR = 2
+DEBUG_ERRORNOHALT = 3
 
 function Ananke.core.debug.Add()
 
 	if !Ananke.core.debug.enabled then return end
 	
 	local info = debug.getinfo(2, "nSl")
-	local i = Ananke.core.debug.data[info.short_src][info.linedefined][1] + 1 or 1
+	local i = Ananke.core.debug.data.Stack[info.short_src][info.linedefined][1] + 1 or 1
 	
-	Ananke.core.debug.data[info.short_src] = Ananke.core.debug.data[info.short_src] or {}
-	Ananke.core.debug.data[info.short_src][info.linedefined] = Ananke.core.debug.data[info.short_src][info.linedefined] or {}
-	Ananke.core.debug.data[info.short_src][info.linedefined] = { i , info , i / (os.time() - Ananke.core.debug.timestamp) }
+	Ananke.core.debug.data.Stack[info.short_src] = Ananke.core.debug.data[info.short_src] or {}
+	Ananke.core.debug.data.Stack[info.short_src][info.linedefined] = Ananke.core.debug.data[info.short_src][info.linedefined] or {}
+	Ananke.core.debug.data.Stack[info.short_src][info.linedefined] = { i , info , i / (os.time() - Ananke.core.debug.timestamp) }
 	
 end
 
@@ -22,14 +28,14 @@ function Ananke.core.debug.PrintData(...)
 
 	local args = {...}
 	
-	if args[1] and Ananke.core.debug.data[args[1]] then
-		if args[2] and Ananke.core.debug.data[args[1]][args[2]] then
-			return PrintTable(Ananke.core.debug.data[args[1]][args[2]])
+	if args[1] and Ananke.core.debug.data.Stack[args[1]] then
+		if args[2] and Ananke.core.debug.data.Stack[args[1]][args[2]] then
+			return PrintTable(Ananke.core.debug.data.Stack[args[1]][args[2]])
 		else
-			return PrintTable(Ananke.core.debug.data[args[1]])
+			return PrintTable(Ananke.core.debug.data.Stack[args[1]])
 		end
 	else
-		return PrintTable(Ananke.core.debug.data)
+		return PrintTable(Ananke.core.debug.data.Stack)
 	end
 	
 end
@@ -46,12 +52,33 @@ function Ananke.core.debug.Error( str , Halt )
 	local func = Halt and Error or ErrorNoHalt
 
 	local info = debug.getinfo(2, "nSl")
-	table.insert( Ananke.core.debug.Errors , { str , info } )
+	table.insert( Ananke.core.debug.data.Errors , { str , info } )
 	
 	func( str )
 	
 end
 
+function Ananke.core.debug.Print( str , type )
+
+	if type == DEBUG_PRINT then
+		print( '[Ananke] ' .. str )
+	elseif type == DEBUG_ERROR then
+		error( '[Ananke] ' .. str )
+	elseif type == DEBUG_ERRORNOHALT then
+		ErrorNoHalt( '[Ananke] ' .. str )
+	end
+	
+end
+
+function Ananke.core.debug.Log( str , data, push )
+	
+	table.insert( Ananke.core.debug.data.Logs, { str , data } )
+	
+	if push then
+		Ananke.core.debug.Print( str , push )
+	end
+	
+end
 	
 	
 
