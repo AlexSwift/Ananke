a6ff6d5a412d69677fc1c7cf22f87b7dc4664561
AlexSwift <alex@carlingfordgroup.co.uk>
2014-03-10 17:17:05 +0000

    Added MODULE.Hooks

--- gamemode/modules/modules.lua
+++ gamemode/modules/modules.lua
@@ -10,9 +10,7 @@ class "Ananke.Modules" {
 				
 				print( 'Loading Modules' )
 				for k,v in pairs( tab ) do
-					
-					Ananke.Modules.LoadModule( v , dir )
-					
+					Ananke.Modules.LoadModule( v , dir )	
 				end	
 			end;
 			
@@ -23,33 +21,26 @@ class "Ananke.Modules" {
 				if Ananke._MODULES[ name ] then return end
 				print('\tLoaded module : ' .. name)
 				
-				MODULE = Ananke.Modules.new() --Start MODULE 
+				MODULE = Ananke.Modules.new()
 				for f,func in pairs(Ananke.Modules.functions) do
 					func( dir .. name)
 				end
-				MODULE:Register() --End Module
-				
-				hook.Call( "Ananke.PreModuleLoad", GAMEMODE, MODULE.Name )
+				MODULE:Register()
 				local call = MODULE.Load and MODULE:Load()
-				hook.Call( "Ananke.PostModuleLoad", GAMEMODE, MODULE.Name )
 				
 			end;
 			
 			Initialise = function()
-				for k,v in pairs(_MODULES) do
-					hook.Call( "Ananke.PreModuleLoad", GAMEMODE, v.Name )
+				for k,v in pairs(Ananke._MODULES) do
 					local call = v.Load and v:Load()
-					hook.Call( "Ananke.PostModuleLoad", GAMEMODE, v.Name )
-				
 				end
 			end;
 			
 			Get = function( name )
-				
 				if Ananke._MODULES[ name ] then 
 					return Ananke._MODULES[ name ]
 				else
-					--Ananke.core.debug.Error( 'Module not found', false )
+					Ananke.core.debug.Error( 'Module not found', false )
 				end
 			end;
 			
@@ -62,10 +53,33 @@ class "Ananke.Modules" {
 	
 	protected {
 	
+		Name = 'module_name';
+		Author = 'module_author';
+		Contact = 'name@domaine.com';
+		Website = 'www.website.com';
+		Description = 'module_description';
+		
+		Hooks = {};
+	
 		Register = function( self )
 			Ananke._MODULES[ self.Name ] = self
+			
+			for k,v in pairs( self:GetHooks() ) do 
+			
+				hook.Add( k , self.Name ..':' .. k , function( ... )
+					local args = {...}
+					v( unpack( args ) )
+				end)
+				
+				print('\t\tRegistered Hook : ' .. k )
+				
+			end
 
 		end;
+		
+		GetHooks = function( self )
+			return self.Hooks 
+		end
 	};
 	
 	private {
@@ -88,12 +102,6 @@ class "Ananke.Modules" {
 			};
 			
 		};
-		
-		Name = 'module_name';
-		Author = 'module_author';
-		Contact = 'name@domaine.com';
-		Website = 'www.website.com';
-		Description = 'module_description'
 		
 	};
 }
