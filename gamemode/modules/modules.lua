6a176b1a97b8ddcc8434ef2c3ea03cc9d80a7078
AlexSwift <alex@carlingfordgroup.co.uk>
2014-03-11 18:40:11 +0000

    Updated how modules work. EVERYTHING BROKE
    http://puu.sh/7qFyQ.txt

--- gamemode/modules/modules.lua
+++ gamemode/modules/modules.lua
@@ -12,28 +12,89 @@ class "Ananke.Modules" {
 				for k,v in pairs( tab ) do
 					Ananke.Modules.LoadModule( v , dir )	
 				end	
+				
 			end;
 			
 			LoadModule = function( name, dir )
 			
-				dir = dir or Ananke.Name .. "/gamemode/modules/"
+				print('\tLoading module : ' .. name)
+			
+				local dir = dir or Ananke.Name .. "/gamemode/modules/"
+				local INI
+				local info
 				
-				if Ananke._MODULES[ name ] then return end
-				print('\tLoaded module : ' .. name)
+				if not file.Exists( dir .. name ..'/info.ini' , 'LUA' ) and not CLIENT then
+					Error( '/t/tFailed to load module ' .. name .. ' as info file was not found!' )
+					return
+				end
+				
+				INI = INIParer.new( )
+				INI:LoadFile( dir .. name ..'/info.ini' , 'LUA' )
+				INI = INI:Parse( )
+				
+				if Ananke._MODULES[ INI['Info'].Name ] then
+					Error( '/t/tFailed to load module '.. name .. ' as module is already loaded ' )
+					return 
+				end
 				
 				MODULE = Ananke.Modules.new()
-				for f,func in pairs(Ananke.Modules.functions) do
-					func( dir .. name)
+				
+				MODULE:SetInfo( INI['Info'] )
+				MODULE:SetFiles( { ['server'] = INI['server'], ['client'] = INI['client'] } )
+				
+				for k,v in ipairs( MODULE:GetFiles()['client'] ) do
+				
+					if CLIENT then
+						Ananke.Include( dir .. name .. '/' .. v )
+					else
+						Ananke.AddCSLuaFile(  dir .. name .. '/' .. v )
+					end
+					
 				end
+				
+				for k,v in ipairs( MODULE:GetFiles()['server'] and MODULE:GetFiles()['server'] ) do
+					Ananke.Include( dir .. name .. '/' .. v )
+				end
+				
+				for k,v in pairs( file.Find( dir .. name .. '/entities/*' , 'LUA' , 'nameasc' ) ) do
+					ENT = {}
+					if CLIENT then
+						Ananke.Include( dir .. name .. '/entities/' .. v .. '/cl_init.lua' )
+					else
+						Ananke.Include( dir .. name .. '/entities/' .. v .. '/init.lua' )
+						Ananke.AddCSLuaFile( dir .. name .. '/entities/' .. v .. '/cl_init.lua' )
+					end
+					scripted_ents.Register( ENT, v )
+					ENT = nil
+				end
+				
+				for k,v in pairs( file.Find( dir .. name .. '/weapons/*' , 'LUA' , 'nameasc' ) ) do
+					SWEP = {}
+					if CLIENT then
+						Ananke.Include( dir .. name .. '/weapons/' .. v .. '/cl_init.lua' )
+					else
+						Ananke.Include( dir .. name .. '/weapons/' .. v .. '/init.lua' )
+						Ananke.AddCSLuaFile( dir .. name .. '/weapons/' .. v .. '/cl_init.lua' )
+					end
+					weapons.Register( SWEP, v )
+					SWEP = nil
+				end
+			
+				for k,v in pairs( file.Find( dir .. name .. '/effects/*' , 'LUA' , 'nameasc' ) ) do
+					EFFECT = {}
+					if CLIENT then
+						Ananke.Include( dir .. name .. '/effects/' .. v .. '/init.lua' )
+					else
+						Ananke.AddCSLuaFile( dir .. name .. '/effects/' .. v .. '/init.lua' )
+					end
+					effects.Register( SWEP, v )
+					EFFECT = nil
+				end
+				
 				MODULE:Register()
+				
 				local call = MODULE.Load and MODULE:Load()
 				
-			end;
-			
-			Initialise = function()
-				for k,v in pairs(Ananke._MODULES) do
-					local call = v.Load and v:Load()
-				end
 			end;
 			
 			Get = function( name )
@@ -53,20 +114,16 @@ class "Ananke.Modules" {
 	
 	protected {
 	
-		Name = 'module_name';
-		Author = 'module_author';
-		Contact = 'name@domaine.com';
-		Website = 'www.website.com';
-		Description = 'module_description';
-		
+		Info = {};
+		Files = {};
 		Hooks = {};
 	
 		Register = function( self )
-			Ananke._MODULES[ self.Name ] = self
+			Ananke._MODULES[ self.Info.Name ] = self
 			
 			for k,v in pairs( self:GetHooks() ) do 
 			
-				hook.Add( k , self.Name ..':' .. k , function( ... )
+				hook.Add( k , self.Info.Name ..':' .. k , function( ... )
 					local args = {...}
 					v( unpack( args ) )
 				end)
@@ -79,31 +136,26 @@ class "Ananke.Modules" {
 		
 		GetHooks = function( self )
 			return self.Hooks 
+		end;
+		
+		SetInfo = function( self, Info )
+			self.Info = Info
+		end;
+		
+		GetInfo = function( self )
+			return self.Info and self.Info or false
+		end;
+		
+		SetFiles = function( self, tab )
+			self.Files = tab
 		end
-	};
-	
-	private {
 		
-		static {
-			functions = {
-			
-				['init.lua'] = function(p)
-					if !SERVER then return end
-					Ananke.include(p .. "/init.lua")
-				end,
-						
-				['cl_init.lua'] = function(p)
-					if SERVER then
-						Ananke.AddCSLuaFile(p .. '/cl_init.lua')
-					else
-						Ananke.include(p .. "/cl_init.lua")
-					end
-				end		
-			};
-			
-		};
+		GetFiles = function( self )
+			return self.Files and self.Files
+		end
 		
 	};
+
 }
 
 
