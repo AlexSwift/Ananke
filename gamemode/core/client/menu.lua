5eddc5f51407fb53758337f3e408bec89cea1a23
Canadialad <mrabey11@gmail.com>
2014-03-26 17:48:39 -0400

    Updates to Menu
    - menu.lua:
    o Cleaned up Ananke.core.Menu files
    o Static functions to create, draw, etc.
    o RenderStack uses Enums to enforce layers
    
    - uiBase.lua
    o Changed the name for the time being
    o The construct function no longer takes a parent, use SetParent after
    creation
    o Uses Ananke.core.Menu:Register in order to be created
    
    - uiModel.lua:
    o Changed name for the time being
    o Uses Ananke.core.Menu:Register() for creation purposes
    
    - uiText.lua:
    o Changed name for time being
    o Uses Ananke.core.Menu:Register for creation purposes
    
    - dm_draw.lua:
    o Updated calls to Ananke.core.Menu for error fixes

--- gamemode/core/client/menu.lua
+++ gamemode/core/client/menu.lua
@@ -1,79 +1,83 @@
 class 'Ananke.core.Menu' {
-	
 	private {
 	
-		__constructor = function( self , name , parent )
-			self.object = core.menu.gui[name].new()
-			if parent then
-				self:SetParent( parent )
-			end
-
-			obj:Init( )
-			table.insert( core.menu.Elements , obj )
+		__constructor = function(self) -- To prevent instantiation of Ananke.core.Menu
 		end;
-
-		object = nil; --Insert user data type here.
 		
 		static {
-		
 			Enabled = false;
-			MousePos = { x = 0 , y = 0 };
-			ActiveElements = {};
-			Objects = {};
 			
+			Objects = {};
+			RenderStack = {};
 		};
 		
 	};
 	
 	protected {
 	
-		Create = new;
-		
-		GetObjects = function( )
-			return self.Objects 
-		end;
-		
-		GetActiveElements = function()
-			local enabled = {}
-			for k,v in pairs(self.Objects) do
-				if self.Objects[k]:IsEnabled() then
-					table.insert( enabled, self.ActiveElements )
-				end
-			end
-			
-		end;
-		
-		Enable = function( )
-			GAMEMODE:PreDrawMenu( )
-			self.Enabled = true
-		end;
-		
-		Disable = function( )
-			GAMEMODE:PostDrawMenu()
-			self.Enabled = false
-		end;
-		
 	};
 	
 	public {
 	
 		static {
-			
+			MousePos = { x = 0 , y = 0 };
+		
 			Initialize = function( self )
-
-				Ananke.core:IncludeDir( 'client' , Ananke.Name .. "/gamemode/core/client/gui/*.lua", "LUA" )
-			
+				self.RenderStack = RenderStack.new()
+				
+				Ananke.core:IncludeDir( 'client' , Ananke.Name .. "/gamemode/core/client/gui")
 			end;
 			
-			renderStack = {}
-		
-		};
-		
-	};
+			Register = function(self, obj, name)
+				self.Objects[name] = obj
+			end;
 			
-}
+			Create = function(self, name, parent)
+				if !self.Objects[name] then return nil end
+				
+				local obj = self.Objects[name].new()
+				
+				obj:Init()
+				
+				if parent then 
+					obj:SetParent(parent)
+				end
+				
+				return obj
+			end;
+			
+			Draw = function(self)
+				self.RenderStack:Draw()
+			end;
+			
+			GetActiveMenus = function(self)
+				local menus = {}
+				
+				for k,v in RenderStack do
+					table.insert(menus, v)
+				end
+				
+				return menus
+			end;
+			
+			Enable = function( )
+				GAMEMODE:PreDrawMenu( )
+				self.Enabled = true
+			end;
+			
+			Disable = function( )
+				GAMEMODE:PostDrawMenu()
+				self.Enabled = false
+			end;
+		
+			IsEnabled = function(self)
+				return self.Enabled
+			end;
+		};
+	};
+};
 
-class "Ananke.core.menu.renderStack" {
+class "RenderStack" {
 	
 	private {
 		layers = {}
@@ -81,27 +85,28 @@ class "Ananke.core.menu.renderStack" {
 	
 	public {
 		__constructor = function(self)
-			for i = 1, 5 do
+			for i = 1, 4 do
 				layers[i] = LinkedList.new()
 			end
 		end;
 		
 		Draw = function(self)
-			-- loop through 'layers' calling Draw on each object
-			for k,v in pairs(self['layers']) do
-				for node in v:Iterate() do
+			for i = 1, 4 do
+				for node in layers[i]:Iterate() do
 					node:Draw()
 				end
 			end
 		end;
 		
-		-- TODO: layer is enum value
 		Add = function(self, obj, layer)
+			if Enums['UILAYERS'][layer] == nil then return end
+			
 			self['layers'][layer]:AddHead(obj)
 		end;
 		
-		-- TODO: layer is enum value
 		Remove = function(self, obj, layer)
+			if Enums['UILAYERS'][layer] == nil then return end
+		
 			self['layers'][layer]:RemoveByValue(obj)
 		end;
 	};
--- gamemode/hooks/gm_draw.lua
+++ gamemode/hooks/gm_draw.lua
@@ -1,17 +1,17 @@
 if not CLIENT then return end
 
 hook.Add( 'HUDPaint' , 'Ananke.core.menu.draw' , function( )
-	if not Ananke.core.menu.Enabled then return end
+	if not Ananke.core.Menu:IsEnabled() then return end
 	
 	local mp = { x = 0 , y = 0 }
 	
 	if gui.MousePos() ~= mp then
-		for k,v in pairs(Anenke.core.menu.ActiveElements) do
+		for k,v in pairs(Ananke.core.menu:GetActiveMenus()) do
 			v:OnCursorMoved()
 		end
 	end
 
-	core.menu.MousePos = mp
+	Ananke.core.Menu.MousePos = mp
 	mp = nil; -- __gc
 
 	for i = 1 , #elements do
