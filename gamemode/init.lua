9ed778b9c7129a901b026ce2ac484f2912a2827e
LennyPenny <l.bernhardt@live.de>
2014-04-12 16:54:39 +0200

    ~temp~ add support for viewmodelhands in base

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -6,3 +6,21 @@ AddCSLuaFile( "extentions/extentions.lua" )
 
 include('shared.lua')
 
+
+function GM:PlayerSpawn( ply )
+	-- Your code
+	ply:SetupHands() -- Create the hands and call GM:PlayerSetHandsModel
+end
+
+-- Choose the model for hands according to their player model.
+function GM:PlayerSetHandsModel( ply, ent )
+
+	local simplemodel = player_manager.TranslateToPlayerModelName( ply:GetModel() )
+	local info = player_manager.TranslatePlayerHands( simplemodel )
+	if ( info ) then
+		ent:SetModel( info.model )
+		ent:SetSkin( info.skin )
+		ent:SetBodyGroups( info.body )
+	end
+
+end
