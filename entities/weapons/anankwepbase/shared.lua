66183926943250d30ba4f7c3024b36c022fee48d
LennyPenny <l.bernhardt@live.de>
2014-04-13 23:20:29 +0200

    starting weaponbase2

---
+++ entities/weapons/anankwepbase/shared.lua
@@ -0,0 +1,22 @@
+--[[ anankwepbase by the Ananke team ~ ]]
+
+--[[Author info]]
+SWEP.PrintName = "anankwepbase"
+SWEP.Author = "Lenny., Ananke Team"
+SWEP.Instructions = "Make gunz with this"
+
+--[[Spawn info]] --only used for debugging in sandbox
+SWEP.Spawnable = true
+SWEP.AdminOnly = false 
+
+
+--[[]]----[[]]--[[SWEP Vars]]--[[]]----[[]]--
+
+SWEP.UseHands = true
+
+SWEP.ViewModel = "models/weapons/cstrike/c_rif_ak47.mdl"
+SWEP.WorldModel = "models/weapons/w_rif_ak47.mdl"
+
+SWEP.ViewModelFOV = 70
+SWEP.HoldType = "smg"
+
