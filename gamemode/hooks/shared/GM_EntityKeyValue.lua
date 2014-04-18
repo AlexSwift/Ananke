cc2ace9aed3a032eef0233745f3b0e3080890431
AlexSwift <alex@carlingfordgroup.co.uk>
2014-04-18 21:49:45 +0100

    Fixed KeyValues

--- gamemode/hooks/shared/GM_EntityKeyValue.lua
+++ gamemode/hooks/shared/GM_EntityKeyValue.lua
@@ -1,3 +1,33 @@
+local KeyValues = {} -- [ent] = { key = value }
+
 function GM:EntityKeyValue( ent, key, value )
+	
+	if not KeyValues[ ent ] then KeyValues[ ent ] = {} end
+	
+	KeyValues[ ent ][ key ] = value
 
 end
+
+function FindMetaTable( 'Entity' ):GetKeyValue( key )
+
+	if not KeyValues[ self ] then
+		return nil
+	elseif not KeyValues[ self ][ key ] then
+		return nil
+	end
+	
+	return KeyValues[ self ][ key ]
+	
+end
+
+function FindMetaTable( 'Entity' ):GetKeyValues( )
+	
+	if not KeyValues[ self ] then
+		return { }
+	end
+	
+	return KeyValues[ self ]
+	
+end
+
+	
